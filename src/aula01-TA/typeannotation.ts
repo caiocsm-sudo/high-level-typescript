83a0686d4d78b53f017c1851da4dbca40d4d01e1
caiocsm-sudo <stampedesw29@gmail.com>
2023-09-18 20:44:47 -0300

    types 2

--- src/aula01-TA/typeannotation.ts
+++ src/aula01-TA/typeannotation.ts
@@ -1,12 +1,22 @@
 /* eslint-disable */
 
 // basic types
-let nome: string = 'vai tomar no cu';
+let nome: string = "vai tomar no cu";
 let age: number = 30;
 let adult: boolean = true; // only true or false
-let smb: symbol = Symbol('any symbol');
+let smb: symbol = Symbol("any symbol");
+let gargamel: string = 'bitches'; let putaqueopariu: number = 200;
 // let big: bigint = 10n;
 
+// arrays
 let numberArray: Array<number> = [1, 2, 3, 4, 5, 6, 78, 19];
 let strArray: string[] = ["typescript", "Type", "Annotations"];
+
+// objects
+
+let person: {
+  name: string;
+  age: number;
+  adult?: boolean;
+};
 
