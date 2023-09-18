c4efad0a5b4bf894a1d465cd43ae36b370811141
caiocsm-sudo <stampedesw29@gmail.com>
2023-09-18 17:01:30 -0300

    types

--- src/aula01-TA/typeannotation.ts
+++ src/aula01-TA/typeannotation.ts
@@ -7,3 +7,6 @@ let adult: boolean = true; // only true or false
 let smb: symbol = Symbol('any symbol');
 // let big: bigint = 10n;
 
+let numberArray: Array<number> = [1, 2, 3, 4, 5, 6, 78, 19];
+let strArray: string[] = ["typescript", "Type", "Annotations"];
+
