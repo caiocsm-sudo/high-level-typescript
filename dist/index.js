a007cbf9dfb6bb294c864ae38492239732efa27e
caiocsm-sudo <stampedesw29@gmail.com>
2023-09-18 16:46:36 -0300

    typescript studies

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+const pogramas = 'pogramas';
+console.log(pogramas);
+exports.default = pogramas;
---
+++ src/aula01-TA/typeannotation.ts
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+// basic types
+let nome: string = 'vai tomar no cu';
+let age: number = 30;
+let adult: boolean = true; // only true or false
+let smb: symbol = Symbol('any symbol');
+// let big: bigint = 10n;
+
